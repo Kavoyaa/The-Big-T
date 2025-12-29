ecd34be9338b711d176ee42096ed78c1cc5be1ce
Kavya Pandey <78526936+Kavoyaa@users.noreply.github.com>
2025-12-29 17:50:30 +0530

    DOORSSSS (might be buggy af gang)

---
+++ Assets/ClassroomSceneLoad.cs
@@ -0,0 +1,16 @@
+using UnityEngine;
+
+public class ClassroomSceneLoad : MonoBehaviour
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        DataManager.Instance.playerLocation = "ClassroomScene";
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Assets/NightSceneLoad.cs
@@ -0,0 +1,16 @@
+using UnityEngine;
+
+public class NightSceneLoad : MonoBehaviour
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        DataManager.Instance.playerLocation = "NightScene";
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
