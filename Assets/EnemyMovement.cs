96fe7bc5c936b9031c87b8805f37679a1bf151c3
Kavya Pandey <78526936+Kavoyaa@users.noreply.github.com>
2025-12-29 12:00:25 +0530

    End game if The Big T gets too close

--- Assets/EnemyMovement.cs
+++ Assets/EnemyMovement.cs
@@ -1,12 +1,14 @@
 using System.Collections;
 using UnityEngine;
 using UnityEngine.AI;
+using UnityEngine.SceneManagement;
 
 public class EnemyMovement : MonoBehaviour
 {
     public Transform target;
     public NavMeshAgent enemy;
     public float updateSpeed = 0.1f;
+    public float killDistance = 10f;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
@@ -18,6 +20,12 @@ public class EnemyMovement : MonoBehaviour
     void Update()
     {
         enemy.SetDestination(target.position);
+        float distance = Vector3.Distance(target.position, transform.position);
+
+        if (distance < killDistance)
+        {
+            SceneManager.LoadScene("MainMenu");
+        }
     }
 
 
