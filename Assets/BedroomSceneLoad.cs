a7d53f1fb7e88c4d8d512295b9636a1b03a34b29
Kavya Pandey <78526936+Kavoyaa@users.noreply.github.com>
2025-12-30 13:46:53 +0530

    Implement bedroom

---
+++ Assets/BedroomSceneLoad.cs
@@ -0,0 +1,16 @@
+using UnityEngine;
+
+public class BedroomSceneLoad : MonoBehaviour
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        DataManager.Instance.playerLocation = "Bedroom";
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
